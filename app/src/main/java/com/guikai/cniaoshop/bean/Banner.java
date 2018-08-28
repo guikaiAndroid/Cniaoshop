47db681971a540efa817fa69d56a6d332cdfa45a
guikai <916126728@qq.com>
2018-08-28 08:44:38 +0800

    okHttp加载banner

---
+++ app/src/main/java/com/guikai/cniaoshop/bean/Banner.java
@@ -0,0 +1,40 @@
+package com.guikai.cniaoshop.bean;
+
+/*
+ * Time:         2018/8/27 23:24
+ * Package_Name: com.guikai.cniaoshop.bean
+ * File_Name:    Banner
+ * Creator:      Anding
+ * Note:         首页轮播图实体类
+ */
+public class Banner extends BaseBean {
+
+    //根据获取的Json的数据 写一个相对于的实体类
+    private String name;
+    private String imageUrl;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- app/src/main/java/com/guikai/cniaoshop/fragment/HomeFragment.java
+++ app/src/main/java/com/guikai/cniaoshop/fragment/HomeFragment.java
@@ -10,23 +10,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.guikai.cniaoshop.R;
 import com.guikai.cniaoshop.adapter.DividerItemDecortion;
 import com.guikai.cniaoshop.adapter.HomeCatgoryAdapter;
+import com.guikai.cniaoshop.bean.Banner;
 import com.guikai.cniaoshop.bean.HomeCategory;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -37,9 +42,11 @@ public class HomeFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private HomeCatgoryAdapter mAdatper;
 
-
-
     private static final  String TAG="HomeFragment";
+
+    private Gson mGson = new Gson();
+
+    private List<Banner> mBanner;
 
     @Nullable
     @Override
@@ -49,35 +56,42 @@ public class HomeFragment extends Fragment {
         mSliderLayout = (SliderLayout) view.findViewById(R.id.slider);
 
         requestImages();
-//        initSlider();
-//
-//
-//
-//        initRecyclerView(view);
+
+        initRecyclerView(view);
         return view;
     }
 
     private void requestImages() {
-        String url = "http://112.124.22.238:8081/course_api/banner/query?type=1";
+        String url = "http://112.124.22.238:8081/course_api/banner/query";
 
         OkHttpClient client = new OkHttpClient();
 
+        //okHttp3跟2不同 这里使用FormBody
+        FormBody body = new FormBody.Builder()
+                .add("type", "1")
+                .build();
+
         Request request = new Request.Builder()
                 .url(url)
+                .post(body)
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, IOException e) {
-                Log.e("HomeFragment", "xxxxxxxxx");
+                Log.e("xxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxx");
             }
 
             @Override
             public void onResponse(@NonNull Call call, Response response) throws IOException {
                 if (response.isSuccessful()) {
                     String json = response.body().string();
-                    Log.e("HomeFragment", "json="+json );
+                    Log.e("xxxxxxx","json"+json);
+                    //将Json数据转换为List<Banner> 利用Gson Type转换
+                    Type type = new TypeToken<List<Banner>>(){}.getType();
+                    mBanner = mGson.fromJson(json, type);
 
+                    initSlider();
                 }
             }
         });
@@ -142,56 +156,31 @@ public class HomeFragment extends Fragment {
     //引入轮播图github框架
     private void initSlider() {
 
-        TextSliderView textSliderView = new TextSliderView(this.getContext());
-        textSliderView.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t2416/102/20949846/13425/a3027ebc/55e6d1b9Ne6fd6d8f.jpg");
-        textSliderView.description("新品推荐");
-        textSliderView.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
-            @Override
-            public void onSliderClick(BaseSliderView baseSliderView) {
+        if (mBanner != null) {
+            for (Banner banner : mBanner) {
 
-                Toast.makeText(HomeFragment.this.getActivity(),"新品推荐",Toast.LENGTH_LONG).show();
+                TextSliderView textSliderView = new TextSliderView(this.getContext());
 
+                textSliderView.image(banner.getImageUrl());
+                textSliderView.description(banner.getName());
+                textSliderView.setScaleType(BaseSliderView.ScaleType.Fit);
+                mSliderLayout.addSlider(textSliderView);
             }
-        });
+        }
 
-        TextSliderView textSliderView2 = new TextSliderView(this.getActivity());
-        textSliderView2.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1507/64/486775407/55927/d72d78cb/558d2fbaNb3c2f349.jpg");
-        textSliderView2.description("时尚男装");
-
-
-        textSliderView2.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
-            @Override
-            public void onSliderClick(BaseSliderView baseSliderView) {
-
-                Toast.makeText(HomeFragment.this.getActivity(),"时尚男装",Toast.LENGTH_LONG).show();
-
-            }
-        });
-
-
-        TextSliderView textSliderView3 = new TextSliderView(this.getActivity());
-        textSliderView3.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1363/77/1381395719/60705/ce91ad5c/55dd271aN49efd216.jpg");
-        textSliderView3.description("家电秒杀");
-
-
-        textSliderView3.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
-            @Override
-            public void onSliderClick(BaseSliderView baseSliderView) {
-
-                Toast.makeText(HomeFragment.this.getActivity(),"家电秒杀",Toast.LENGTH_LONG).show();
-            }
-        });
-
-        mSliderLayout.addSlider(textSliderView);
-        mSliderLayout.addSlider(textSliderView2);
-        mSliderLayout.addSlider(textSliderView3);
 
         //原点动画
         mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+
+        mSliderLayout.setCustomAnimation(new DescriptionAnimation());
         //图片跳转动画
         mSliderLayout.setPresetTransformer(SliderLayout.Transformer.Default);
         //时间
         mSliderLayout.setDuration(3000);
+
+
+
+
 
         mSliderLayout.addOnPageChangeListener(new ViewPagerEx.OnPageChangeListener() {
             @Override
