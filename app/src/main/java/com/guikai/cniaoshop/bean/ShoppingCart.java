b627279d9956e945553af5e15961422bd73eb31c
unknown <guikai@droi.com>
2018-09-19 17:03:46 +0800

    添加购物车SharePreference存储工具类

---
+++ app/src/main/java/com/guikai/cniaoshop/bean/ShoppingCart.java
@@ -0,0 +1,25 @@
+package com.guikai.cniaoshop.bean;
+
+import java.io.Serializable;
+
+public class ShoppingCart extends Wares implements Serializable {
+
+    private int count;
+    private boolean isChecked = true;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+}
---
+++ app/src/main/java/com/guikai/cniaoshop/utils/CartProvider.java
@@ -0,0 +1,95 @@
+package com.guikai.cniaoshop.utils;
+
+
+import android.content.Context;
+import android.util.SparseArray;
+
+import com.google.gson.reflect.TypeToken;
+import com.guikai.cniaoshop.bean.ShoppingCart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//提供购物车数据的类
+public class CartProvider {
+
+    public static final String CART_JSON = "cart_json";
+
+    private SparseArray<ShoppingCart> datas = null;
+
+    private Context mContext;
+
+    public CartProvider(Context context) {
+        datas = new SparseArray<>(10);
+        mContext = context;
+        listToSparse();
+    }
+
+    //写入数据 参数为一个商品实体类
+    public void put(ShoppingCart cart) {
+
+        //写入数据之前 我们需要判断购物车是否此商品存在 如果存在+1 否则新增商品
+        ShoppingCart temp = datas.get(cart.getId().intValue());
+
+        if (temp != null) {
+            temp.setCount(temp.getCount()+1);
+        } else {
+            temp = cart;
+            temp.setCount(1);
+        }
+        datas.put(cart.getId().intValue(),temp);
+        commit();
+    }
+
+    public void update(ShoppingCart cart) {
+        datas.put(cart.getId().intValue(),cart);
+        commit();
+    }
+
+    //删除数据
+    public void delete(ShoppingCart cart) {
+        datas.delete(cart.getId().intValue());
+        commit();
+    }
+
+    //获取所有数据
+    public List<ShoppingCart> getAll() {
+        return getDataFromLocal();
+    }
+
+    //从SharePreference中读取数据
+    public List<ShoppingCart> getDataFromLocal() {
+        String json = PreferencesUtils.getString(mContext, CART_JSON);
+        List<ShoppingCart> carts = null;
+        if (json != null) {
+            carts = JSONUtil.fromJson(json, new TypeToken<List<ShoppingCart>>(){}.getType());
+        }
+        return carts;
+    }
+
+    public void commit() {
+        List<ShoppingCart> carts = sparseToList();
+        PreferencesUtils.putString(mContext,CART_JSON, JSONUtil.toJSON(carts));
+    }
+
+    public List<ShoppingCart> sparseToList() {
+
+        int size = datas.size();
+        List<ShoppingCart> list = new ArrayList<>(size);
+        for (int i = 0; i < size ; i++) {
+            list.add(datas.valueAt(i));
+        }
+        return list;
+    }
+
+    //将list转化为Sparese
+    private void listToSparse() {
+        List<ShoppingCart> carts = getDataFromLocal();
+
+        if (carts != null && carts.size()> 0) {
+            for (ShoppingCart cart :carts) {
+                datas.put(cart.getId().intValue(),cart);
+            }
+        }
+    }
+}
