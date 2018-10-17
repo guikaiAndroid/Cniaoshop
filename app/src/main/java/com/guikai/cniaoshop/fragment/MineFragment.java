c013a05648a207462aa400b12eda346bac485e92
Gui-kai <916126728@qq.com>
2018-10-17 15:00:05 +0800

    我的页面coding

--- app/src/main/java/com/guikai/cniaoshop/fragment/MineFragment.java
+++ app/src/main/java/com/guikai/cniaoshop/fragment/MineFragment.java
@@ -1,19 +1,22 @@
 package com.guikai.cniaoshop.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.guikai.cniaoshop.R;
 
-public class MineFragment extends Fragment {
-    @Nullable
+public class MineFragment extends BaseFragment {
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mine,container,false);
+    public View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mine, container,false);
+    }
+
+    @Override
+    public void init() {
+
     }
 }
