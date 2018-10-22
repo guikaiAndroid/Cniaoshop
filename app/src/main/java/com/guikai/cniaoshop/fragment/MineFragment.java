b01c517596ef35e527018c886672f755d89ddf14
guikai <916126728@qq.com>
2018-10-22 23:57:34 +0800

    login user

--- app/src/main/java/com/guikai/cniaoshop/fragment/MineFragment.java
+++ app/src/main/java/com/guikai/cniaoshop/fragment/MineFragment.java
@@ -3,15 +3,19 @@ package com.guikai.cniaoshop.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.guikai.cniaoshop.CniaoApplication;
 import com.guikai.cniaoshop.Contants;
 import com.guikai.cniaoshop.LoginActivity;
 import com.guikai.cniaoshop.R;
+import com.guikai.cniaoshop.bean.User;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,6 +35,7 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
     private void initView(View view) {
         mImageHead = (CircleImageView) view.findViewById(R.id.img_head);
         mTexUserName = (TextView) view.findViewById(R.id.text_username);
+        mbtnLogout = view.findViewById(R.id.btn_logout);
         mImageHead.setOnClickListener(this);
         mTexUserName.setOnClickListener(this);
     }
@@ -38,7 +43,8 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
 
     @Override
     public void init() {
-
+        User user = CniaoApplication.getmInstance().getUser();
+        showUser(user);
     }
 
     @Override
@@ -47,7 +53,10 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
             case R.id.img_head:
             case R.id.text_username:
                 toLogin();
-
+                break;
+            case R.id.btn_logout:
+                toLogout();
+                break;
         }
     }
 
@@ -56,8 +65,27 @@ public class MineFragment extends BaseFragment implements View.OnClickListener{
         startActivityForResult(intent,Contants.REQUEST_CODE);
     }
 
+    private void toLogout() {
+        CniaoApplication.getmInstance().clearUser();
+        showUser(null);
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        User user = CniaoApplication.getmInstance().getUser();
+        showUser(user);
+    }
 
+    private void showUser(User user) {
+        if (user != null) {
+            if (!TextUtils.isEmpty(user.getLogo_url())){
+                mTexUserName.setText(user.getUsername());
+                Picasso.with(getActivity()).load(user.getLogo_url()).into(mImageHead);
+                mbtnLogout.setVisibility(View.VISIBLE);
+            }
+        } else {
+            mTexUserName.setText(R.string.to_login);
+            mbtnLogout.setVisibility(View.GONE);
+        }
     }
 }
