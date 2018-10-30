37ad9a08348283f797891a4585f8e301e2e76cf5
guikai <916126728@qq.com>
2018-10-31 00:20:48 +0800

    null

--- app/src/main/java/com/guikai/cniaoshop/RegActivity.java
+++ app/src/main/java/com/guikai/cniaoshop/RegActivity.java
@@ -1,46 +1,159 @@
 package com.guikai.cniaoshop;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.guikai.cniaoshop.utils.ManifestUtil;
+import com.guikai.cniaoshop.utils.ToastUtils;
+import com.guikai.cniaoshop.widget.ClearEditText;
+import com.guikai.cniaoshop.widget.CnToolbar;
 import com.mob.MobSDK;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cn.smssdk.EventHandler;
 import cn.smssdk.SMSSDK;
-import cn.smssdk.gui.RegisterPage;
+import cn.smssdk.utils.SMSLog;
+import dmax.dialog.SpotsDialog;
 
 public class RegActivity extends AppCompatActivity {
+
+    public static final String TAG = "RegActivity";
+
+    // 默认使用中国区号
+    private static final String DEFAULT_COUNTRY_ID = "42";
+
+    private CnToolbar mToolBar;
+    private TextView mTxtCountry;
+    private TextView mTxtCountryCode;
+    private ClearEditText mEtxtPhone;
+    private ClearEditText mEtxtPwd;
+    private SpotsDialog dialog;
+
+    private SMSEvenHanlder evenHanlder;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
-        MobSDK.init(this, "283f45086d140", "0f06c27175cde81917cdc1539d5fb8bb");
-        sendCode(this);
+        initView();
+
+        MobSDK.init(this, ManifestUtil.getMetaDataValue(this, "Mob-AppKey"),
+                ManifestUtil.getMetaDataValue(this,"Mob-AppSecret"));
+
+        evenHanlder = new SMSEvenHanlder();
+        SMSSDK.registerEventHandler(evenHanlder);
+
+        String[] country = SMSSDK.getCountry(DEFAULT_COUNTRY_ID);
+        if (country != null) {
+
+            mTxtCountryCode.setText("+"+country[1]);
+            mTxtCountry.setText(country[0]);
+        }
     }
 
-    public void sendCode(Context context) {
-        RegisterPage page = new RegisterPage();
-        //如果使用我们的ui，没有申请模板编号的情况下需传null
-        page.setTempCode(null);
-        page.setRegisterCallback(new EventHandler() {
-            public void afterEvent(int event, int result, Object data) {
-                if (result == SMSSDK.RESULT_COMPLETE) {
-                    // 处理成功的结果
-                    HashMap<String,Object> phoneMap = (HashMap<String, Object>) data;
-                    String country = (String) phoneMap.get("country"); // 国家代码，如“86”
-                    String phone = (String) phoneMap.get("phone"); // 手机号码，如“13800138000”
-                    // TODO 利用国家代码和手机号码进行后续的操作
+    private void initView() {
+        mToolBar = findViewById(R.id.toolbar);
+        mTxtCountry = findViewById(R.id.txtCountry);
+        mTxtCountryCode = findViewById(R.id.txtCountryCode);
+        mEtxtPhone = findViewById(R.id.edittxt_phone);
+        mEtxtPwd = findViewById(R.id.edittxt_pwd);
 
-                } else{
-                    // TODO 处理错误的结果
-                }
+        mToolBar.setRightButtonOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
             }
         });
-        page.show(context);
     }
 
+    class SMSEvenHanlder extends EventHandler {
+        @Override
+        public void afterEvent(final int event, final int result, final Object data) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (result == SMSSDK.RESULT_COMPLETE) {
+                        if (event == SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES) {
+
+                            onCountryListGot((ArrayList<HashMap<String, Object>>) data);
+
+                        } else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
+                            // 请求验证码后，跳转到验证码填写页面
+
+                            afterVerificationCodeRequested((Boolean) data);
+
+                        } else if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+
+                        }
+                    } else {
+
+                        // 根据服务器返回的网络错误，给toast提示
+                        try {
+                            ((Throwable) data).printStackTrace();
+                            Throwable throwable = (Throwable) data;
+
+                            JSONObject object = new JSONObject(
+                                    throwable.getMessage());
+                            String des = object.optString("detail");
+                            if (!TextUtils.isEmpty(des)) {
+                                ToastUtils.show(RegActivity.this, des);
+                                return;
+                            }
+                        } catch (Exception e) {
+                            SMSLog.getInstance().w(e);
+                        }
+
+                    }
+
+
+                }
+            });
+        }
+    }
+
+    private void onCountryListGot(ArrayList<HashMap<String, Object>> countries) {
+        // 解析国家列表
+        for (HashMap<String, Object> country : countries) {
+            String code = (String) country.get("zone");
+            String rule = (String) country.get("rule");
+            if (TextUtils.isEmpty(code) || TextUtils.isEmpty(rule)) {
+                continue;
+            }
+            Log.d(TAG,"code="+code + "rule="+rule);
+        }
+
+    }
+
+    /** 请求验证码后，跳转到验证码填写页面 */
+    private void afterVerificationCodeRequested(boolean smart) {
+
+
+
+        String phone = mEtxtPhone.getText().toString().trim().replaceAll("\\s*", "");
+        String code = mTxtCountryCode.getText().toString().trim();
+        String pwd = mEtxtPwd.getText().toString().trim();
+
+        if (code.startsWith("+")) {
+            code = code.substring(1);
+        }
+
+        Intent intent = new Intent(this,RegSecondActivity.class);
+        intent.putExtra("phone",phone);
+        intent.putExtra("pwd",pwd);
+        intent.putExtra("countryCode",code);
+
+        startActivity(intent);
+    }
 }
